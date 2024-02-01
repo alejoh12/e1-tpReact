e9aefa5f1c7053d65d91d0ae61ed59b819aaf797
Alejo Herrera <alejoherrera1166@gmail.com>
2024-02-01 07:14:55 -0300

    Mensaje agregado.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Mensaje from './components/Mensaje'
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
   return (
     <>
-      
+      <Mensaje></Mensaje>
     </>
   )
 }
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,9 @@
+const Mensaje = () => {
+    return (
+        <section>
+            <h1 className="text-center text-primary my-4 display-2">Hola Mundillo!</h1>
+        </section>
+    );
+};
+
+export default Mensaje;
